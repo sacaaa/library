d880ce8404c972e647a4b2c22786395549d6d71c
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-11-07 13:57:07 +0100

    initial commit

---
+++ server/src/test/java/inf/unideb/hu/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package inf.unideb.hu.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
