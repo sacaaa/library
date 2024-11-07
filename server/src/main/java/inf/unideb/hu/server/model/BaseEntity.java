efbc5e136c981475780badc4bea9edabbe2ca6b7
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-11-07 14:32:43 +0100

    add: base entity

---
+++ server/src/main/java/inf/unideb/hu/server/model/BaseEntity.java
@@ -0,0 +1,23 @@
+package inf.unideb.hu.server.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+}
